bf10a8d462dc08f2a9fee8638240a417605ffcb1
strikerop96 <106062852+strikerop96@users.noreply.github.com>
2022-05-25 16:19:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ async def helpstr(message: types.Message):
 async def helpstr(message: types.Message):
     await message.answer_chat_action("typing")
     await message.reply(
-        "<b>STRIKERCHECKER COMMANDS</b> <b>➣ Stripe Charge/Auth [✅]</b> <code>/chk cc|mm|yy|cvv</code> <b>➣ Check SK Key [✅]</b> <code>/key sk_live</code> <b>➣ Check Info [✅]</b> <code>/info</code> <b>➣ Check BIN Info [✅]</b> <code>/bin xxxxxx</code> Contact → @strikerop95 <b>Bot By @strikermarket</b>"
+        "<b>STRIKERCHECKER COMMANDS</b>\n<b>➣ Stripe Charge/Auth [✅]</b> <code>/chk cc|mm|yy|cvv</code>\n<b>➣ Check SK Key [✅]</b> <code>/key sk_live</code>\n<b>➣ Check Info [✅]</b> <code>/info</code>\n<b>➣ Check BIN Info [✅]</b> <code>/bin xxxxxx</code>\n<b>Contact → @strikerop95</b>\n<b>Bot By @strikermarket</b>"
     )
 
     
