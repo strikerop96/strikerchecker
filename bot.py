c11c3dd9917c7968e4eb8d817b0d3e22af3263ad
strikerop96 <106062852+strikerop96@users.noreply.github.com>
2022-05-27 11:41:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -161,7 +161,7 @@ async def ch(message: types.Message):
       "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
     }
       
-    ad = session.post("https://www.mrchecker.net/card-checker/ccn2/api.php/"
+    ad = session.post("https://www.mrchecker.net/card-checker/ccn2/api.php/",
                      data=cc, header=heads)
     
     toc = time.perf_counter()
