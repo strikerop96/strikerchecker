42b77c84146f36f7fcbcead1236b0c8634510a6d
strikerop96 <106062852+strikerop96@users.noreply.github.com>
2022-05-27 08:17:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -162,7 +162,7 @@ async def ch(message: types.Message):
       "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
     }
       
-    ad = session.post("https://www.mrchecker.net/card-checker/ccn2/api.php/",
+    ad = session.post("https://www.mrchecker.net/card-checker/ccn2/api.php",
                      data=cc, headers=headerss)
     
     if 'Live' in ad.text :
